62a69f45058064eb22bc110f5407d48811a140c1
VedanshSaha <76149219+VedanshSaha@users.noreply.github.com>
2022-01-10 20:14:49 +0530

    Update data.py

--- data.py
+++ data.py
@@ -1,3 +1,4 @@
+import pandas as pd
 df = pd.read_csv("data.csv")
 mean = df.groupby(["student_id", "level"], as_index=False)["attempt"].mean()
 fig = px.scatter(mean, x="student_id", y="level", size="attempt", color="attempt")
